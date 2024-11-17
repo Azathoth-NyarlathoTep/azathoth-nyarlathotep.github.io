22f1aeefabcf8edd86a989fe94a9680bb1028559
M o o n. <moon353844@gmail.com>
2024-11-17 06:19:44 +0000

    cs61b update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731654841',resources: [
+    cacheName: 'chirpy-1731822827',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
